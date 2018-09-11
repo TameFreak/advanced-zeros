1b05253c9b58a0f5515e7c21193c65f6a0fa43fb
Alex Lesnicky <alex.lesnicky@gmail.com>
2018-09-11 22:48:23 +0300

    not done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  let counter = 0;
+
+
+  let countTwo = 0;
+  let countFive = 0;
+
+  let div = base;
+  while(div % 5 === 0){
+    countFive++;
+    div = div / 5;
+  }
+
+  while(div % 2 === 0){
+    countTwo++;
+    div = div / 2;
+  }
+
+  div = (div === 1) ? (countFive === 0) ? 2 : 5 : div;
+
+  for (let i = 2; i <= number; i += 1){
+    let elem = i;
+    while(elem % div === 0){
+      counter++;
+      elem = elem / div;
+    }
+  }
+
+  if (div !== 5 && div !== 2) return Math.trunc(counter);
+  return (div === 5) ? Math.trunc(counter / countFive) : Math.trunc(counter / countTwo);
 }
